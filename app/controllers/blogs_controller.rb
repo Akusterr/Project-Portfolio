2dc880c3213c1c207250c52f021114a006c9736c
Alexa <kusteralexa@gmail.com>
2023-07-18 09:21:20 -0400

    Built pages with controller generator

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs or /blogs.json
   def index
