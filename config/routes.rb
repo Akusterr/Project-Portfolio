687ee0e789da7942960f64533e159b8a91fd0150
Alexa <kusteralexa@gmail.com>
2023-07-19 09:28:19 -0400

    integrated index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
