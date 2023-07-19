d5a6c8f6dfa18494051f493494c6c6a41ae50c9a
Alexa <kusteralexa@gmail.com>
2023-07-19 10:29:39 -0400

    Integrated new and create actions for portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
+  resources :portfolios
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
