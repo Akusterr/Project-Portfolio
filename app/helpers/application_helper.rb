08de784ee3ef5c8754281b183d6c09fe25a674ad
Alexa <kusteralexa@gmail.com>
2023-08-05 10:09:23 -0400

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
     def login_helper
-        if current_user 
-            link_to "logout", destroy_user_session_path, method: :delete
-        else
+        if current_user.is_a?(GuestUser)
             (link_to "register", new_user_registration_path) +
             "<br>".html_safe +
             (link_to "login", new_user_session_path)
+        else
+            link_to "logout", destroy_user_session_path, method: :delete
         end
     end
 
